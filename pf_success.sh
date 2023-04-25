716094a5c3c371f75accbeb902a1b58631e980b4
Julius Lehmann <git@devpi.de>
2023-04-25 10:48:29 +0200

    Add port forwarding

--- pf_success.sh
+++ pf_success.sh
@@ -21,5 +21,16 @@ if [ -n "$PF_DEST_IP" ] && [ -n "$FWD_IFACE" ]; then
   echo "$(date): Forwarding incoming VPN traffic on port $1 to $PF_DEST_IP:$1"
 fi
 
+# Map port to another fixed port
+if [ -n "$MAP_PORT"]; then
+  iptables -A INPUT -i wg0 -p tcp --dport "$MAP_PORT" -j ACCEPT
+  iptables -A INPUT -i wg0 -p udp --dport "$MAP_PORT" -j ACCEPT
+  echo "$(date): Allowing incoming traffic on map port $MAP_PORT"
+  iptables -A PREROUTING -t nat -i wg0 -p tcp --dport "$1" -j REDIRECT --to-port "$MAP_PORT"
+  iptables -A PREROUTING -t nat -i wg0 -p udp --dport "$1" -j REDIRECT --to-port "$MAP_PORT"
+  echo $(date): Forwarding traffic from port "$1" to "$MAP_PORT"
+fi
+
+
 # Run another user-defined script if defined and present
 [ -n "$PORT_SCRIPT" ] && [ -x "$PORT_SCRIPT" ] && echo "$(date): Running user-defined script: $PORT_SCRIPT" && eval "$PORT_SCRIPT $1" &
